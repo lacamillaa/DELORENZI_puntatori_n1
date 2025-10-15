d5fbb9b581e41c447b7f616be40c2a252663ce3d
lacamillaa <12999356@itisrossi.vi.it>
2025-10-15 11:17:02 +0200

    made string input possible

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    // lettura della stringa (max. 100 caratteri)
+    char* str = malloc(101 * sizeof(char));
+    int i = 0;
+    for (i = 0; i < 101; i++) {
+        char c = (char) getchar();
+        if (c >= 'A' && c <= 'Z') {
+            c += 32;
+        }
+        if (c >= 'a' && c <= 'z') {
+            str[i] = c;
+        }
+        if (c == '\n') {
+            str[i] = 0;
+            str = (char*) realloc(str, i * sizeof(char));
+            if (str == NULL) {
+                printf("Out of memory\n");
+                return 1;
+            }
+            break;
+        }
+    }
+    // elaborazione
+    // ...
+    // deallocazione
+    free(str);
+    str = NULL;
     return 0;
 }
