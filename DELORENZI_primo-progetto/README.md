e3c2d176208348433608da2342e426d9d02e3bc0
lacamillaa <12999356@itisrossi.vi.it>
2026-01-28 11:31:49 +0100

    fix #1

--- DELORENZI_primo-progetto/README.md
+++
@@ -1,4 +0,0 @@
-# Il mio primo progetto
-### Migliori classi
-* 4AII
-* 4BII
--- DELORENZI_primo-progetto/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("Hello, World!\n");
-    int n = 0;
-    scanf("%d", &n);
-    printf("Numero: %d", n);
-    return 0;
-}
